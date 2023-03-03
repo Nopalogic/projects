5a3868483e46e6f4e1847b0568302716d29540b0
Nopalogic <naufaladhi02@gmail.com>
2023-03-04 00:46:45 +0700

    first commit

--- faq-collapse/script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll('.toggle');
-
-buttons.forEach((button) => {
-	button.addEventListener('click', () => {
-		button.parentNode.classList.toggle('active');
-	});
-});

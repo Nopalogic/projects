43bc8e3dc5540c457e37448d537707f02961a6b1
Nopalogic <naufaladhi02@gmail.com>
2023-06-01 09:48:41 +0700

    modify

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+// const images = document.getElementById('images')
+// const leftBtn = document.getElementById('left')
+// const rightBtn = document.getElementById('right')
+// const image = document.querySelectorAll('#images img')
+
+// let idx = 0
+// let interval = setInterval(run, 2000)
+
+// function run() {
+//     idx++
+//     changeImage()
+// }
+
+// function changeImage() {
+//     if(idx > image.length - 1) {
+//         idx = 0
+//     } else if(idx < 0) {
+//         idx = image.length - 1
+//     }
+
+//     images.style.transform = `translateX(${-idx * 1024}px)`
+// }
+
+// function resetInterval() { 
+//     clearInterval(interval)
+//     interval = setInterval(run, 2000)
+// }
+
+// rightBtn.addEventListener('click', () => {
+//     idx++
+//     changeImage()
+//     resetInterval()
+// })
+
+// leftBtn.addEventListener('click', () => {
+//     idx--
+//     changeImage()
+//     resetInterval()
+// })
